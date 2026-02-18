612248c27946af2657bc37b80cadc2368b5e9ca7
Himani78116 <himanibagale@gmail.com>
2026-02-18 11:04:22 +0000

    Adding index.html, style.css and script.js files.

---
+++ script.js
@@ -0,0 +1,54 @@
+const task = document.getElementById("input");
+let add_btn = document.getElementById("add-btn");
+let tasklist = document.getElementById("taskList");
+
+add_btn.addEventListener("click", () => {
+    if (task.value === "") {
+        alert("Please enter a task");
+    }
+    else {
+        const li = document.createElement("li");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("custom-checkbox");
+
+        const span = document.createElement("span");
+        span.textContent = task.value;
+        span.classList.add("task-text");
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.classList.add("delete-btn");
+
+
+        li.append(checkbox, span, deleteBtn);
+        tasklist.appendChild(li);
+
+        task.value = "";
+        saveData();
+    }
+});
+
+tasklist.addEventListener("click", (event) => {
+    if(event.target.tagName === "BUTTON") {
+        event.target.parentElement.remove();
+        saveData();
+    }
+
+    else if (event.target.type === "checkbox") {
+        const span = event.target.parentElement.querySelector(".task-text");
+        span.classList.toggle("completed");
+        saveData();
+    }
+});
+
+function saveData() {
+    localStorage.setItem("data",tasklist.innerHTML);
+}
+
+function displayData() {
+    tasklist.innerHTML = localStorage.getItem("data");
+}
+
+displayData();
